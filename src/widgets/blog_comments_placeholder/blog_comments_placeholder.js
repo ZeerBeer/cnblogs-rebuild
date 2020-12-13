5a87fc2265d55682f64159c64c1e720995145cae
zeerbeer <zeerbeer@qq.com>
2020-12-13 10:00:32 +0800

    optimize comment

--- src/widgets/blog_comments_placeholder/blog_comments_placeholder.js
+++ src/widgets/blog_comments_placeholder/blog_comments_placeholder.js
@@ -11,24 +11,37 @@ export function blog_comments_placeholder() {
                 document.querySelector("#tip_comment2").nextElementSibling.innerHTML = ``
 
 
+
                 comments.querySelectorAll(".feedbackItem").forEach(comment => {
+
+                    comment.querySelector(".comment_vote").innerHTML = ``
+
                     // console.log(comment)
                     const feedbackListSubtitle = comment.querySelector(".feedbackListSubtitle")
                     const comment_date = feedbackListSubtitle.querySelector(".comment_date")
 
                     const date = comment_date.innerHTML
                     const id = comment_date.nextElementSibling.innerHTML
+                    const idUrl = comment_date.nextElementSibling.getAttribute("href")
                     const layer = comment.querySelector(".layer").innerHTML
+                    const layerPos = comment.querySelector(".layer").getAttribute("href")
                     const avatar = comment.querySelector(".comment_vote")?.nextElementSibling.innerHTML
 
+                    const op = comment.querySelector(".feedbackManage").innerHTML
 
                     feedbackListSubtitle.innerHTML = `
                         <img class="avatar" src="${avatar}"/>
-                        <div>
-                            <div>${id}</div>
-                            <div style="display: flex; flex-direction: row">
-                                <div>${date}</div>
-                                <div>${layer}</div>
+                        <div style="width: 100%">
+                            <a href="${idUrl}">${id}</a>
+                            <div style="
+                            display: flex;
+                            flex-direction: row;
+                            color: #b3b3b3;
+                            font-size: .75em;
+                            ">
+                                <div style="padding-right: .5em; padding-bottom: .2em; padding-top: .2em">${date}</div>
+                                <div style="padding: .2em .5em;" href="${layerPos}">${layer}</div>
+                                ${op}
                             </div>
                         </div>
                     `
